650ba0965d9cb475bbab1b9b0408fd5635aede5b
EruelUrsua <eruelursua@gmail.com>
2025-06-03 09:59:55 +0800

    added working logic

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 
+import java.util.Scanner;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
@@ -8,10 +9,47 @@ public class Main {
 
 
     public static void main(String[] args) {
+    
+        double numC = 0, numU = 0, grade = 0, average = 0, total = 0;
+        int numS = 0, num;
+        Scanner input = new Scanner(System.in);
+
+        do { 
+            System.out.println("\nEnter number of Courses: ");
+             numS = input.nextInt();
+            if (numS <=0)
+            {
+                    System.out.println("Invalid Input!");
+            }
+
+
+        } while (numS <=0);
         
-    setFrame();
+        for(num = 0; num<numS; num++){
+
+            int c = num + 1;
+            System.out.println("Enter the grade of course number "+c+":");
+            grade = input.nextDouble();
+            total = getTotal(grade, total);
+
+
+        }
+
+        double sub = numS;
+
+        average = total / sub ;
+
+        System.err.println("Number of Courses: " + numS);
+        System.err.println("Average GPA: " + average);
 
     }
+
+    static double getTotal(double a , double b)
+    {
+        return  a + b;
+    }
+
+
     static void setFrame(){
 
     JFrame frame = new JFrame();
@@ -21,6 +59,7 @@ public class Main {
         label.setVerticalAlignment(JLabel.CENTER);
         label.setHorizontalAlignment(JLabel.CENTER);
 
+        
 
         frame.setTitle("Hello World");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
